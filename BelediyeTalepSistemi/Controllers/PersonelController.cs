8cf48e8b37b99836986ce91c6ae9d5605734382a
Ahmet Can Kurupınar <cankurupinar@gmail.com>
2026-07-17 14:03:16 +0300

    Personel paneli ve talep detay ekranı eklendi

--- BelediyeTalepSistemi/Controllers/PersonelController.cs
+++ BelediyeTalepSistemi/Controllers/PersonelController.cs
@@ -1,14 +1,46 @@
-﻿using BelediyeTalepSistemi.Helpers;
+﻿using BelediyeTalepSistemi.Data;
+using BelediyeTalepSistemi.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BelediyeTalepSistemi.Controllers
 {
+    [RoleAuthorize(Roles.Personel)]
     public class PersonelController : Controller
     {
-        [RoleAuthorize(Roles.Personel)]
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public PersonelController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var talepler = await _context.Talepler
+                .Include(t => t.ApplicationUser)
+                .Include(t => t.Mudurluk)
+                .Include(t => t.TalepDurumu)
+                .OrderByDescending(t => t.OlusturulmaTarihi)
+                .ToListAsync();
+
+            return View(talepler);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var talep = await _context.Talepler
+                .Include(t => t.ApplicationUser)
+                .Include(t => t.Mudurluk)
+                .Include(t => t.TalepDurumu)
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (talep == null)
+            {
+                return NotFound();
+            }
+
+            return View(talep);
         }
     }
 }
