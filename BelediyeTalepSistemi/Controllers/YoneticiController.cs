3096e4c3380e87e1c9c3653d614480ec290dac1e
Ahmet Can Kurupınar <cankurupinar@gmail.com>
2026-07-20 10:15:49 +0300

    Yönetici paneli ve talep detay ekranı eklendi

--- BelediyeTalepSistemi/Controllers/YoneticiController.cs
+++ BelediyeTalepSistemi/Controllers/YoneticiController.cs
@@ -1,14 +1,57 @@
-﻿using BelediyeTalepSistemi.Helpers;
+﻿using BelediyeTalepSistemi.Data;
+using BelediyeTalepSistemi.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BelediyeTalepSistemi.Controllers
 {
+    [RoleAuthorize(Roles.Yonetici)]
     public class YoneticiController : Controller
     {
-        [RoleAuthorize(Roles.Yonetici)]
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public YoneticiController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.ToplamTalep = await _context.Talepler.CountAsync();
+            ViewBag.YeniTalep = await _context.Talepler
+                .Include(t => t.TalepDurumu)
+                .CountAsync(t => t.TalepDurumu != null && t.TalepDurumu.DurumAdi == "Yeni");
+
+            ViewBag.ToplamVatandas = await _context.ApplicationUsers
+                .CountAsync(u => u.Rol == Roles.Vatandas);
+
+            ViewBag.ToplamPersonel = await _context.ApplicationUsers
+                .CountAsync(u => u.Rol == Roles.Personel);
+
+            var talepler = await _context.Talepler
+                .Include(t => t.ApplicationUser)
+                .Include(t => t.Mudurluk)
+                .Include(t => t.TalepDurumu)
+                .OrderByDescending(t => t.OlusturulmaTarihi)
+                .ToListAsync();
+
+            return View(talepler);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var talep = await _context.Talepler
+                .Include(t => t.ApplicationUser)
+                .Include(t => t.Mudurluk)
+                .Include(t => t.TalepDurumu)
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (talep == null)
+            {
+                return NotFound();
+            }
+
+            return View(talep);
         }
     }
 }
