2aac06aa81b06c151236963fa7cf8bafb3032eaf
Ahmet Can Kurupınar <cankurupinar@gmail.com>
2026-07-21 11:27:40 +0300

    Müdürlük yönetimi ve atama sistemi eklendi

---
+++ BelediyeTalepSistemi/Controllers/MudurlukController.cs
@@ -0,0 +1,122 @@
+﻿using BelediyeTalepSistemi.Data;
+using BelediyeTalepSistemi.Helpers;
+using BelediyeTalepSistemi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BelediyeTalepSistemi.Controllers
+{
+    [RoleAuthorize(Roles.Yonetici)]
+    public class MudurlukController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MudurlukController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var mudurlukler = await _context.Mudurlukler
+                .Include(m => m.Talepler)
+                .ToListAsync();
+
+            return View(mudurlukler);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Mudurluk mudurluk)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(mudurluk);
+            }
+
+            _context.Mudurlukler.Add(mudurluk);
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = "Müdürlük başarıyla eklendi.";
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var mudurluk = await _context.Mudurlukler.FindAsync(id);
+
+            if (mudurluk == null)
+            {
+                return NotFound();
+            }
+
+            return View(mudurluk);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Mudurluk mudurluk)
+        {
+            if (id != mudurluk.Id)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(mudurluk);
+            }
+
+            _context.Mudurlukler.Update(mudurluk);
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = "Müdürlük başarıyla güncellendi.";
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var mudurluk = await _context.Mudurlukler
+                .Include(m => m.Talepler)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (mudurluk == null)
+            {
+                return NotFound();
+            }
+
+            return View(mudurluk);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var mudurluk = await _context.Mudurlukler
+                .Include(m => m.Talepler)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (mudurluk == null)
+            {
+                return NotFound();
+            }
+
+            if (mudurluk.Talepler.Any())
+            {
+                TempData["ErrorMessage"] = "Bu müdürlüğe bağlı talepler olduğu için silme işlemi yapılamaz.";
+                return RedirectToAction("Index");
+            }
+
+            _context.Mudurlukler.Remove(mudurluk);
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = "Müdürlük başarıyla silindi.";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BelediyeTalepSistemi/Controllers/YoneticiController.cs
+++ BelediyeTalepSistemi/Controllers/YoneticiController.cs
@@ -1,6 +1,7 @@
 ﻿using BelediyeTalepSistemi.Data;
 using BelediyeTalepSistemi.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BelediyeTalepSistemi.Controllers
@@ -18,6 +19,7 @@ namespace BelediyeTalepSistemi.Controllers
         public async Task<IActionResult> Index()
         {
             ViewBag.ToplamTalep = await _context.Talepler.CountAsync();
+
             ViewBag.YeniTalep = await _context.Talepler
                 .Include(t => t.TalepDurumu)
                 .CountAsync(t => t.TalepDurumu != null && t.TalepDurumu.DurumAdi == "Yeni");
@@ -52,6 +54,56 @@ namespace BelediyeTalepSistemi.Controllers
             }
 
             return View(talep);
+        }
+
+        public async Task<IActionResult> AssignMudurluk(int id)
+        {
+            var talep = await _context.Talepler
+                .Include(t => t.ApplicationUser)
+                .Include(t => t.Mudurluk)
+                .Include(t => t.TalepDurumu)
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (talep == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Mudurlukler = new SelectList(
+                await _context.Mudurlukler.ToListAsync(),
+                "Id",
+                "MudurlukAdi",
+                talep.MudurlukId
+            );
+
+            return View(talep);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssignMudurluk(int id, int mudurlukId)
+        {
+            var talep = await _context.Talepler.FindAsync(id);
+
+            if (talep == null)
+            {
+                return NotFound();
+            }
+
+            var mudurlukVarMi = await _context.Mudurlukler.AnyAsync(m => m.Id == mudurlukId);
+
+            if (!mudurlukVarMi)
+            {
+                TempData["ErrorMessage"] = "Seçilen müdürlük bulunamadı.";
+                return RedirectToAction("Index");
+            }
+
+            talep.MudurlukId = mudurlukId;
+
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = "Talebin müdürlüğü başarıyla güncellendi.";
+
+            return RedirectToAction("Index");
         }
     }
 }
